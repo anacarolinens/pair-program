7636eb394e8eaa624c29c522fbd367e5974c173a
Ana Caroline <caroline.ana.n.s@gmail.com>
2023-07-05 20:18:32 -0500

    aula 05/07/2023

--- project-favorites/api/index.js
+++ project-favorites/api/index.js
@@ -1,26 +1,31 @@
 const http = require('http')
 const data = require('./urls.json')
 const URL = require('url')
-
 const fs = require('fs')
 const path = require('path')
 
-    function writeFile(cb) {
+    function writeFile(cb) { //carregando uma funcao
         fs.writeFile(
             path.join(__dirname, 'urls.json'),
             JSON.stringify(data, null, 2),
             err=> {
                 if (err) throw err
-                    cb('Operação realizada com sucesso!')
+                    cb('Operação realizada com sucesso!')//callback que aparece no message
             })
     }
 
 http.createServer((req, res) => {
     const { name, url, del } = URL.parse(req.url, true).query
+    
+    //tratamento do cors
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin':'*'
+    })
+
     if(!name || !url)
         return res.end(JSON.stringify(data))
     if(del){
-        data.urls = data.urls.filter(item => item.url != url)
+        data.urls = data.urls.filter(item => item.url != url) //manipula dentro do objeto e depois manda pro documento Json
         return writeFile(message => res.end(message)
     )}
     data.urls.push({name, url})  
--- project-favorites/public/script.js
+++ project-favorites/public/script.js
@@ -2,21 +2,17 @@ const ul = document.querySelector('ul')
 const input = document.querySelector('input')
 const form = document.querySelector('form')
 
-/*
-
-// Não se preocupem com esse pedaço de código comentado! Vamos descomentá-lo quando tivermos acabado de construir a API.
 
 // Função que carrega o conteúdo da API.
 async function load() {
     // fetch está como await para evitar que entre num esquema de promisse e só devolva o conteúdo após a iteração qua acontece em seguida.
-    const res = await fetch('http://localhost:3000/')
+    const res = await fetch('http://localhost:3000/') //await - faz o codico ficar sincrono, fazendo ele ser exercutado até que ele acabe antes de qualquer outra instrução
         .then(data => data.json())
     // Iterando no vetor com o conteúdo (JSON) que está vindo da API e adicionando-os no frontend.
-    res.urls.map(({name, url}) => addElement({name, url}))
+    res.urls.map(({name, url}) => addElement({name, url})) //indo dentro do urls e fazendo um map para adicionar cada item para dentro da pagina
 }
 
 load()
-*/
 
 function addElement({ name, url }) {
   //seleciona pelo id
@@ -30,34 +26,55 @@ function addElement({ name, url }) {
   btn.textContent = 'X';
   btn.addEventListener('click', function() {
       removeElement(li);
+      
   });
 
   // Cria um novo elemento link
   const a = document.createElement('a');
   a.textContent = name;
   a.href = url;
+      li.appendChild(a);
+    li.appendChild(btn);
 
+    // Adiciona o elemento li à lista ul
+    ul.appendChild(li);
+
+  try {
+    fetch(`http://localhost:3000?name=${nome}&url=${url}, `, {
+      method:'POST'
+    })
+  }catch (err){
+    console.log('Erro!',err)
+
+  }
   // Adiciona o link ao elemento li
-  li.appendChild(a);
-  li.appendChild(btn);
 
-  // Adiciona o elemento li à lista ul
-  ul.appendChild(li);
+
+  
 }
 
-// Exemplo de uso da função
-const link = {
-  name: 'Google',
-  url: 'https://www.google.com'
-};
+// // Exemplo de uso da função
+// const link = {
+//   name: 'Google',
+//   url: 'https://www.google.com'
+// };
 
-addElement(link);
+// addElement(link);
 
 //função para remover o link
 function removeElement(element) {
   if(confirm('Deseja realmente excluir esse link?')){
     const ul = document.getElementById('link-list');
-    ul.removeChild(element);
+    
+    try {
+      ul.removeChild(element);
+      fetch(`http://localhost:3000?name=${element.nome}&url=${element.url}&del=${element.true}`, {
+        method:'DELETE'
+      })
+       
+   }catch(err){
+     console.log('Erro!',err)
+   }
   }
 }
  
@@ -69,7 +86,7 @@ form.addEventListener('submit', (event) => {
     let { value } = input
 
     if (!value)
-        return alert('Preencha o campo!')
+        return alert('Preencha o campo!')//validar o campo
 
     const [name, url] = value.split(',')
 
@@ -77,7 +94,7 @@ form.addEventListener('submit', (event) => {
         return alert('O texto não está formatado da maneira correta.')
 
     if (!/^http/.test(url))
-        return alert('Digite a url da maneira correta.')
+        return alert('Digite a url da maneira correta.')//formatcao do campo
 
     addElement({ name, url })
 
